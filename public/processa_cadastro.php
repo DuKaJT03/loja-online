c1ddcb58437b106622888621e52cdac0800f26f5
DuKaJT03 <jhonticona2001@gmail.com>
2026-02-23 10:14:03 -0300

    processa_cadastro convertido para PDO

--- public/processa_cadastro.php
+++ public/processa_cadastro.php
@@ -10,75 +10,49 @@ use Jhon\Loja\Database\Conexao;
         //capturando os valores do formulário
         $nome = $_POST['nome'];
         $email = $_POST['email'];
-        $senha = ($_POST['senha']); 
+        $senha = $_POST['senha']; 
         $tipo = $_POST['tipo'];
         $confirmar_senha = $_POST['confirmar_senha'];
 
         $conexao = Conexao::conectar();
         
         //Verifica se e-mail já existe 
-        $verifica = $conexao->prepare("SELECT id FROM usuarios WHERE email = ?");
-        $verifica->bind_param("s", $email);
-        $verifica->execute();
-        $verifica->store_result();
+        $verifica = $conexao->prepare(
+            "SELECT id FROM usuarios WHERE email = ?"
+            );
+        $verifica->execute([
+            $email
+        ]);
 
-        if($verifica->num_rows > 0){
+        if($verifica->rowCount() > 0){
             echo "<p class='erro'>E-mail já cadastrado. Use outro. </p>";
             exit;
-        }else if($senha != $confirmar_senha){
+        } 
+        if($senha != $confirmar_senha){
             echo "<p class='erro'>As senhas não coincidem.</p>";
             exit;
         }
-        $senha = password_hash($_POST['senha'], PASSWORD_DEFAULT); //Criptografando
+
+        //Criptografa a senha
+        $senhaHash = password_hash($senha, PASSWORD_DEFAULT);
 
         //Inserindo no banco de dados (INSEGURO)
         //$sql = "INSERT INTO usuarios (nome, email, senha, tipo)VALUES ('$nome', '$email', '$senha', '$tipo')";
         //if($conexao->query($sql)===TRUE){
 
-        $stmt = $conexao->prepare("INSERT INTO usuarios (nome, email, senha, tipo) VALUES (?, ?, ?, ?)");
-        $stmt->bind_param("ssss", $nome, $email, $senha, $tipo);
-
-        if($stmt->execute()){
+        //Insere usuário
+        $stmt = $conexao->prepare(
+            "INSERT INTO usuarios (nome, email, senha, tipo) VALUES (?, ?, ?, ?)"
+            );
+        $stmt->exeute([
+            $nome,
+            $email,
+            $senhaHash,
+            $tipo
+        ]);
             echo "<p style='color:green;'>Usuário cadastrado com sucesso!</p>";
             echo "<a href='login.html'>Ir para o Login</a>";
-        }else{
-            echo "Erro: " . $stmt->error;
-        }
-        $stmt->close();
-        $conexao->close();
-}else{
-    header('Location: index.html');
-    exit;
-}
-/** composer.json
- * bootstrap
- * .env
- * conexao
- * gitignore
- * longin.html
- * processalogin
- * logout
- * painelcliente
- * painelvendedor
- * index.php
- * processacadastro
- * 
- * FALTA
- * adicionarcarrinhoajax
- * adicionarcarrinho
- * cadastroproduto
- * editaproduto
- * excluirproduto
- * finalizarcompra
- * listaproduto
- * menu
- * processaeditaproduto
- * processaproduto
- * removercarrinho
- * vercarrinho
- * veritenspedido
- * verpedidos
- * verprodutos
- * autoload
- */
-?>
+    }else{
+        Header('Location: index.html');
+        exit;
+    }
