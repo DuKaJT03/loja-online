837fde4660ca124fb86638d39256660f405b27f7
DuKaJT03 <jhonticona2001@gmail.com>
2026-02-27 09:40:44 -0300

    login 100% PDO postgres

--- public/processa_login.php
+++ public/processa_login.php
@@ -5,23 +5,23 @@ require_once __DIR__ . '/../vendor/autoload.php';//Inclui a conexão com o banco
 
 use Jhon\Loja\Database\Conexao;
 
-$conexao = Conexao::conectar();
-
 if(isset($_POST['email'], $_POST['senha'])){ //verifica se os campos email e senha chegaram
     $email = $_POST['email'];//captura os valores nas variáveis
     $senha = $_POST['senha'];
+
+    $pdo = Conexao::conectar();
     /*
     $sql = "SELECT id, nome, senha, tipo FROM usuarios WHERE email = '$email'";//monta o sql para buscar o usuário pelo email
     $resultado = $conexao->query($sql);//executa o SQL
     */
-    $stmt = $conexao->prepare("SELECT id, nome, senha, tipo FROM usuarios WHERE email = ?");
-    $stmt->bind_param("s", $email);
+    $sql = "SELECT id, nome, senha, tipo FROM usuarios WHERE email = :email";
+    $stmt = $pdo->prepare($sql);
+    $stmt->bindValue(':email', $email);
     $stmt->execute();
 
-    $resultado = $stmt->get_result();
+    $usuario = $stmt->fetch(PDO::FETCH_ASSOC);
 
-    if($resultado->num_rows == 1){//verifica se retornou 1 resultado
-        $usuario = $resultado->fetch_assoc(); //recebe o resultado convertido em array associativo (chave => valor)
+    if($usuario){//verifica se retornou 1 resultado
 
         if(password_verify($senha, $usuario['senha'])){ //$senha conrresponde com o hash armazenado no banco ($usuario['senha'])
             //Criando ovariáveis de sessão $_SESSION é uma super global do PHP
@@ -47,11 +47,8 @@ if(isset($_POST['email'], $_POST['senha'])){ //verifica se os campos email e sen
     }else{
         echo"Email não encontrado.";
     }
-
-    $stmt->close();
 }else{
     echo"Preencha todos os campos.";
 }
 
-$conexao->close();
 ?>
